8ecfffb9cad87d96f9a832d5c0cbb9c41f01e995
Yu, Jieqiong | Mer | PAYSD <jieqiong.yu@rakuten.com>
2017-11-28 13:49:35 +0900

    first commit

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Yu, Jieqiong | Mer | FINAD on 11/28/29 H.
+//  Copyright © 29 Heisei Yu, Jieqiong | Mer | FINAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
